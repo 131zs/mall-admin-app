b8610552553c75bd56b5e3320a37e33dd4a5f5ad
zs <544565585@qq.com>
2023-05-16 10:57:35 +0800

    初始布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './plugins/ant-design-vue'
+import '@/assets/css/reset.less'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+ 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false //严格命名规则关闭
 })
